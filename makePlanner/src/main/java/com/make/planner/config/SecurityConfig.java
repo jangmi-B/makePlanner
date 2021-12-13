68aa7b831421cf3c57da9931722faeec1415d968
백장미 <jangmi@hivelab.co.kr>
2021-12-13 12:05:34 +0900

    [수정] 로그아웃

--- makePlanner/src/main/java/com/make/planner/config/SecurityConfig.java
+++ makePlanner/src/main/java/com/make/planner/config/SecurityConfig.java
@@ -3,21 +3,15 @@ package com.make.planner.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.make.planner.member.service.MemberService;
-import com.make.planner.provider.CustomAuthenticationProvider;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -64,13 +58,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/")
                 .loginProcessingUrl("/login")
                 .defaultSuccessUrl("/hello")
-                .failureUrl("/?error=true") 
+                .failureUrl("/?error") 
                 .permitAll()
             .and() // 로그아웃 설정
                 .logout()
+                .permitAll()
+//                .logoutUrl("/logout")
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
+                .deleteCookies("JESSIONID", "remember-me")
+//                .addLogoutHandler(new LogoutHandler() {
+//					@Override
+//					public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+//						HttpSession session = request.getSession();
+//						session.invalidate();
+//					}
+//				})
+//                .logoutSuccessHandler(new LogoutSuccessHandler() {
+//					@Override
+//					public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+//							throws IOException, ServletException {
+//						response.sendRedirect("/");
+//					}
+//				})
             .and()
                 // 403 예외처리 핸들링
                 .exceptionHandling()
--- makePlanner/src/main/java/com/make/planner/login/LoginController.java
+++ makePlanner/src/main/java/com/make/planner/login/LoginController.java
@@ -3,11 +3,7 @@ package com.make.planner.login;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.make.planner.member.model.Member;
 import com.make.planner.member.service.MemberService;
 
 @Controller
@@ -21,22 +17,16 @@ public class LoginController {
 		return "/login";
 	}
 	
+	@GetMapping("/logout/result")
+	public String logout() {
+		return "/login";
+	}
+	
 	@GetMapping("/hello")
 	public String main() {
 		return "/main";
 	}
 
-//	@RequestMapping(value = "/loginProc", method = {RequestMethod.POST})
-//	@PostMapping("/loginProc")
-//	public String login() {
-//		return "/main";
-//	}
-	
-	@GetMapping("/logout")
-	public String logout() {
-		return "/";
-	}
-	
 	@GetMapping("/denied")
 	public String denied() {
 		return "/main";
